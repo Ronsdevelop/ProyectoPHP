4722a53415b1c012a4d70b52d71bd856fc2b6ae6
Ronsdevelop <rony_aguilera@hotmail.com>
2020-07-10 09:38:13 -0500

    prueba en modal

--- vistas/categoria.php
+++ vistas/categoria.php
@@ -27,12 +27,15 @@ require 'header.php ';
   <div class="content">
         <div class="container-fluid">
 
-
+       <!--card tabla -->
           <div class="card">
+                <!--card-header -->
             <div class="card-header">
               <h3 class="card-title">Listado de Categorias</h3>
             </div>
             <!-- /.card-header -->
+
+                <!--card-body -->
             <div class="card-body">
               <table id="tbllistado" class="table table-bordered table-striped">
                 <thead class="bg-dark">
@@ -61,12 +64,20 @@ require 'header.php ';
             </div>
             <!-- /.card-body -->
           </div>
-          <div class="row">
+              <!--/.card tabla-->
+        
+    
+     
+ 
+        </div><!-- /.container-fluid -->
+
+        <div class="container-fluid">
+              <div class="row">
           <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-secondary">
                   Agregar Categoria
                 </button>
           </div>
-        </div><!-- /.container-fluid -->
+              </div>
 
         <div class="modal fade" id="modal-secondary">
         <div class="modal-dialog">
@@ -77,7 +88,40 @@ require 'header.php ';
                 <span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
-              <p>One fine body&hellip;</p>
+          <!-- form start -->
+          <form role="form">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
             </div>
             <div class="modal-footer justify-content-between">
               <button type="button" class="btn btn-outline-light" data-dismiss="modal">Cerrar</button>
