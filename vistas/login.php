87776691ff8dff5e377ac3a2ac2da164fddbfd45
Ronsdevelop <rony_aguilera@hotmail.com>
2020-07-12 19:06:54 -0500

    cambio en login

--- vistas/login.php
+++ vistas/login.php
@@ -1,58 +1,25 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="../public/login/css/main.css">
-    <!-- Font-icon css-->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Login</title>
-  </head>
-  <body>
-    <section class="material-half-bg">
-      <div class="cover"></div>
-    </section>
-    <section class="login-content">
-      <div class="logo">
-          <img src="../public/login/img/logo.png" width="300" height="150" alt="logo"/>
-
-      </div>
-      <div class="login-box">
-        <form class="login-form" action="categoria.php">
-          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>INICIAR SESION</h3>
-          <div class="form-group">
-            <label class="control-label">USERNAME</label>
-            <input class="form-control" type="text" placeholder="Usuario" autofocus>
-          </div>
-          <div class="form-group">
-            <label class="control-label">PASSWORD</label>
-            <input class="form-control" type="password" placeholder="Contraseña">
-          </div>
-          <div class="form-group">
-            <div class="utility">
-              <div class="animated-checkbox">
-                <label>
-                  <input type="checkbox"><span class="label-text">Siempre Conectado</span>
-                </label>
-              </div>
-                 
+    <head>
+        <title>Sistema Panaderia Leos</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../public/login/css/login.css ">
+    </head>
+    <body  >
+        <form class="login-form" action="../vistas/inicio.php" method="POST">
+            <div class="login-form__logo-container">
+                <img class="login-form__logo" src="../public/login/img/logo.png" alt="Logo">
             </div>
-          </div>
-          <div class="form-group btn-container">
-            <button class="btn btn-panaderia btn-block"><i class="fa fa-sign-in fa-lg fa-fw"></i>INGRESAR</button>
-          </div>
+            <div class="login-form__content">
+                <div class="login-form__header">Iniciar Sesion</div>
+                <input class="login-form__input" type="text" name="dc_username" placeholder="Usuario">
+                <input class="login-form__input" type="password" name="dc_username" placeholder="Contraseña">
+                <button class="login-form__button" type="submit">Acceder</button>
+                <div class="login-form__links">
+                    <a class="login-form__link" href="./">Olvidaste tu contraseña?</a>
+                </div>
+            </div>
         </form>
-    
-      </div>
-    </section>
-    <!-- Essential javascripts for application to work-->
- 
-    <script src="../public/login/js/jquery-3.2.1.min.js"></script> 
-    <script src="../public/login/js/bootstrap.min.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="../public/login/js/plugins/pace.min.js"></script>
-  
-  </body>
+    </body>
 </html>
