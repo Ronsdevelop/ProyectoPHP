0d4f8887a2a824d05aa38a59c07d6599c676b746
Ronsdevelop <rony_aguilera@hotmail.com>
2020-07-13 10:27:14 -0500

    correcciones en el login

--- vistas/login.php
+++ vistas/login.php
@@ -15,7 +15,7 @@
                 <div class="login-form__header">Iniciar Sesion</div>
                 <input class="login-form__input" type="text" name="dc_username" placeholder="Usuario">
                 <input class="login-form__input" type="password" name="dc_username" placeholder="Contraseña">
-                <button class="login-form__button" type="submit">Acceder</button>
+                <button class="login-form__button" type="submit">ACCEDER</button>
                 <div class="login-form__links">
                     <a class="login-form__link" href="./">Olvidaste tu contraseña?</a>
                 </div>
